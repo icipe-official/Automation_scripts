494a5d0ce6414a3d969c832c6733c2d482df46b1
Mandela M Muithi <digitalcreative@mandelamitau.com>
2023-07-30 01:05:19 +0300

    delete files after extraction

--- installation/repeat_masker_and_modeller/repeat_masker.sh
+++ installation/repeat_masker_and_modeller/repeat_masker.sh
@@ -20,14 +20,17 @@ sudo apt-get install perl
 # Download and install RMBlast
 wget https://www.repeatmasker.org/rmblast/rmblast-2.14.0+-x64-linux.tar.gz
 tar zxvf rmblast-2.14.0+-x64-linux.tar.gz -C /usr/local
+rm rmblast-2.14.0+-x64-linux.tar.gz
 # Download and install Tandem Repeat Finder
 wget https://github.com/Benson-Genomics-Lab/TRF/releases/download/v4.09.1/trf409.linux64
 chmod +x trf409.linux64
 mv trf409.linux64 /usr/local/bin/trf
 # Download and install RepeatMasker
 wget https://www.repeatmasker.org/RepeatMasker/RepeatMasker-4.1.5.tar.gz
-cp RepeatMasker-4.1.5.tar.gz /usr/local
+mv RepeatMasker-4.1.5.tar.gz /usr/local
 cd /usr/local
 gunzip RepeatMasker-4.1.5.tar.gz
 tar xvf RepeatMasker-4.1.5.tar
+sudo rm RepeatMasker-4.1.5.tar
+
 
