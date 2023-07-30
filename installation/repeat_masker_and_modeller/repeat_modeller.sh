e0944a6f233512e82afbcac6796eb4d50720392f
Mandela Mitau <digitalcreative@mandelamitau.com>
2023-07-30 18:57:25 +0300

    update repeat modeller install script

--- installation/repeat_masker_and_modeller/repeat_modeller.sh
+++ installation/repeat_masker_and_modeller/repeat_modeller.sh
@@ -5,26 +5,27 @@ apt-get update -y
 apt-get install -y perl rsync unzip build-essential 
 
 # Install RECON
-cd /usr/local/src/
+cd /usr/local/
 wget http://www.repeatmasker.org/RepeatModeler/RECON-1.08.tar.gz
 tar zxvf RECON-1.08.tar.gz
 cd RECON-1.08/
 make
 
 # Install RepeatScout
-cd /usr/local/src/
+cd /usr/local/
 wget http://www.repeatmasker.org/RepeatScout-1.0.5.tar.gz
 tar zxvf RepeatScout-1.0.5.tar.gz
 cd RepeatScout-1.0.5/
 make
 
 # Install UCSC genome browser command-line utilities
-sudo sudo rsync -aP rsync://hgdownload.soe.ucsc.edu/genome/admin/exe/linux.x86_64/ /usr/local/
-chmod -R +x /usr/local
+mkdir /usr/local/genome_browser_command_line_utilities
+sudo sudo rsync -aP rsync://hgdownload.soe.ucsc.edu/genome/admin/exe/linux.x86_64/ /usr/local/genome_browser_command_line_utilities
+chmod -R +x /usr/local/genome_browser_command_line_utilities
 
 
 # Install LtrHarvest
-cd /usr/local/src/
+cd /usr/local/
 wget http://genometools.org/pub/genometools-1.6.2.tar.gz
 tar zxvf genometools-1.6.2.tar.gz
 cd genometools-1.6.2/
@@ -41,4 +42,8 @@ apt-get install -y mafft
 apt-get install -y cd-hit
 
 # Install Ninja
-# Visit https://github.com/TravisWheelerLab/NINJA/releases/tag/0.95-cluster_only and follow instructions
+cd /usr/local/
+wget https://github.com/TravisWheelerLab/NINJA/archive/refs/tags/0.98-cluster_only.zip
+unzip NINJA-0.98-cluster_only.zip
+cd NINJA-0.98-cluster_only
+make
